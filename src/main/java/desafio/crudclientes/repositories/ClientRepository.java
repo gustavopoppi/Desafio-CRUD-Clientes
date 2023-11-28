a494117ab960b6ef28a95c6dce4fce2b50bffb12
Gustavo Poppi <gustavopoppi@hotmail.com>
2023-11-28 17:39:29 -0300

    [DEV] Cria classe ClientRepository

---
+++ src/main/java/desafio/crudclientes/repositories/ClientRepository.java
@@ -0,0 +1,9 @@
+package desafio.crudclientes.repositories;
+
+import desafio.crudclientes.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Long, Client> {
+}
